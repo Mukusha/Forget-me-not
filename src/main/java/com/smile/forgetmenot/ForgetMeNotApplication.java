55598ecb456b027836f4f470c94b9b7a5b0bea9c
Mukusha <sveta.gurinovitch@yandex.ru>
2023-03-04 15:00:00 +0300

    Spring Initializr

---
+++ src/main/java/com/smile/forgetmenot/ForgetMeNotApplication.java
@@ -0,0 +1,13 @@
+package com.smile.forgetmenot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgetMeNotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForgetMeNotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smile/forgetmenot/ForgetMeNotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smile.forgetmenot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForgetMeNotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
